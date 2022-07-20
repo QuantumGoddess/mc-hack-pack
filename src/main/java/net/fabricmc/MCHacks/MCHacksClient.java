53ce99b428a69b3b352ac280caf3753eb8274fd0
amber.vanraak <amber.vanraak@gmail.com>
2022-07-20 23:40:44 +0200

    cleaned up code and changed fabric config

--- src/main/java/net/fabricmc/MCHacks/MCHacksClient.java
+++ src/main/java/net/fabricmc/MCHacks/MCHacksClient.java
@@ -2,20 +2,17 @@ package net.fabricmc.MCHacks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 
 import org.lwjgl.glfw.GLFW;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.MinecraftClient;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.LiteralTextContent;
-import net.minecraft.text.Text;
 import screens.MCHacksScreen;
 
 public class MCHacksClient implements ClientModInitializer {
