8a7a20375a7fdea8a5da75d0255d63c37c70ec48
QuantumGoddess <amber.vanraak@gmail.com>
2022-07-21 20:29:44 +0200

    changed button layout

--- src/main/java/screens/MCHacksScreen.java
+++ src/main/java/screens/MCHacksScreen.java
@@ -29,35 +29,8 @@ extends Screen {
 
     @Override
     protected void init() {
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 24 + -16, 204, 20, Text.translatable("MCHacks.menuFly" + MCHacks.flyHack.getState()), button -> {
-            MCHacks.flyHack.toggle();
-            
-        }));
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 48 + -16, 98, 20, Text.translatable("MCHacks.menuXray" + MCHacks.xray.getState()), button -> MCHacks.xray.toggle()));
-        this.addDrawableChild(new ButtonWidget(this.width / 2 + 4, this.height / 4 + 48 + -16, 98, 20, Text.translatable("gui.stats"), button -> this.client.setScreen(new StatsScreen(this, this.client.player.getStatHandler()))));
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 96 + -16, 98, 20, Text.translatable("menu.options"), button -> this.client.setScreen(new OptionsScreen(this, this.client.options))));
-        ButtonWidget buttonWidget2 = this.addDrawableChild(new ButtonWidget(this.width / 2 + 4, this.height / 4 + 96 + -16, 98, 20, Text.translatable("menu.shareToLan"), button -> this.client.setScreen(new OpenToLanScreen(this))));
-        buttonWidget2.active = this.client.isIntegratedServerRunning() && !this.client.getServer().isRemote();
-        MutableText text = this.client.isInSingleplayer() ? Text.translatable("menu.returnToMenu") : Text.translatable("menu.disconnect");
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 120 + -16, 204, 20, text, button -> {
-            boolean bl = this.client.isInSingleplayer();
-            boolean bl2 = this.client.isConnectedToRealms();
-            button.active = false;
-            this.client.world.disconnect();
-            if (bl) {
-                this.client.disconnect(new MessageScreen(Text.translatable("menu.savingLevel")));
-            } else {
-                this.client.disconnect();
-            }
-            TitleScreen titleScreen = new TitleScreen();
-            if (bl) {
-                this.client.setScreen(titleScreen);
-            } else if (bl2) {
-                this.client.setScreen(new RealmsMainScreen(titleScreen));
-            } else {
-                this.client.setScreen(new MultiplayerScreen(titleScreen));
-            }
-        }));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 24 + -16, 204, 20, Text.translatable("MCHacks.menuFly" + MCHacks.flyHack.getState()), button -> MCHacks.flyHack.toggle()));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 102, this.height / 4 + 48 + -16, 204, 20, Text.translatable("MCHacks.menuXray" + MCHacks.xray.getState()), button -> MCHacks.xray.toggle()));
     }
 
     @Override
